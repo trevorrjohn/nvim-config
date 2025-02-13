2c05b3a17a10068692cd94111c879de54968294e
Trevor John <trevorrjohn@gmail.com>
2025-02-13 09:48:03 -0500

    Fix typo in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,14 +14,15 @@ return {
       local builtin = require("telescope.builtin")
 
       vim.keymap.set("n", "<Leader>f", builtin.find_files, {})
-      vim.keymap.set("n", "<C-f>", builtin.live_grep, {}),
+      vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
+      vim.keymap.set("i", "<C-f>", builtin.live_grep, {})
       vim.keymap.set("c", "Rg", function()
         builtin.grep_string({ search = vim.fn.input("rg > ") })
       end)
       vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
     end,
     "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && make --build build --config Release'",
+    build = "make -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && make --build build --config Release",
     dependencies = { "smartpde/telescope-recent-files" },
   },
 }
