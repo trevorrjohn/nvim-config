cc396719955061743c17d26954060a4d30902111
Trevor John <trevorrjohn@gmail.com>
2025-07-21 09:30:06 -0400

    Config gopls and fmt on lua lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -3,36 +3,36 @@ local telescope = require("telescope.builtin")
 
 -- Enable LSP-based completion
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		local bufnr = args.buf
-		local map = function(mode, lhs, rhs, desc)
-			vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-		end
+  callback = function(args)
+    local bufnr = args.buf
+    local map = function(mode, lhs, rhs, desc)
+      vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+    end
 
-		-- LSP core
-		vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-		map("n", "K", vim.lsp.buf.hover, "Hover documentation")
-		map("n", "gd", vim.lsp.buf.definition, "Go to definition")
-		map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
-		map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
-		map("n", "gy", vim.lsp.buf.type_definition, "Go to type definition")
-		map("n", "gr", vim.lsp.buf.references, "Find references")
-		map("n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol")
-		map("n", "<leader>ca", vim.lsp.buf.code_action, "Code action")
-		map("n", "<leader>lf", function() vim.lsp.buf.format({ async = true }) end, "Format buffer")
+    -- LSP core
+    vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+    map("n", "K", vim.lsp.buf.hover, "Hover documentation")
+    map("n", "gd", vim.lsp.buf.definition, "Go to definition")
+    map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+    map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
+    map("n", "gy", vim.lsp.buf.type_definition, "Go to type definition")
+    map("n", "gr", vim.lsp.buf.references, "Find references")
+    map("n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+    map("n", "<leader>ca", vim.lsp.buf.code_action, "Code action")
+    map("n", "<leader>lf", function() vim.lsp.buf.format({ async = true }) end, "Format buffer")
 
-		-- Diagnostics
-		map("n", "gl", vim.diagnostic.open_float, "Line diagnostics")
-		map("n", "[d", vim.diagnostic.jump({ count = -1, float = false }), "Previous diagnostic")
-		map("n", "]d", vim.diagnostic.jump({ count = 1, float = false }), "Next diagnostic")
+    -- Diagnostics
+    map("n", "gl", vim.diagnostic.open_float, "Line diagnostics")
+    map("n", "[d", function() vim.diagnostic.jump({ count = -1, float = false }) end, "Previous diagnostic")
+    map("n", "]d", function() vim.diagnostic.jump({ count = 1, float = false }) end, "Next diagnostic")
 
-		-- Telescope-powered LSP browsing
-		map("n", "<leader>fr", telescope.lsp_references, "Telescope: references")
-		map("n", "<leader>fd", telescope.lsp_definitions, "Telescope: definitions")
-		map("n", "<leader>fi", telescope.lsp_implementations, "Telescope: implementations")
-		map("n", "<leader>fs", telescope.lsp_document_symbols, "Telescope: symbols (file)")
-		map("n", "<leader>fS", telescope.lsp_workspace_symbols, "Telescope: symbols (workspace)")
-	end,
+    -- Telescope-powered LSP browsing
+    map("n", "<leader>fr", telescope.lsp_references, "Telescope: references")
+    map("n", "<leader>fd", telescope.lsp_definitions, "Telescope: definitions")
+    map("n", "<leader>fi", telescope.lsp_implementations, "Telescope: implementations")
+    map("n", "<leader>fs", telescope.lsp_document_symbols, "Telescope: symbols (file)")
+    map("n", "<leader>fS", telescope.lsp_workspace_symbols, "Telescope: symbols (workspace)")
+  end,
 })
 
 
@@ -45,6 +45,15 @@ end
 
 -- Setup example servers (manual install required)
 lspconfig.lua_ls.setup({
+  on_attach = function(_, bufnr)
+    -- Format on save for Lua files
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ async = false })
+      end,
+    })
+  end,
   settings = {
     Lua = {
       runtime = { version = "LuaJIT" },
@@ -57,6 +66,17 @@ lspconfig.lua_ls.setup({
     },
   },
 })
+
 lspconfig.ruby_lsp.setup({})
 
-lspconfig.gopls.setup({})
+lspconfig.gopls.setup({
+  on_attach = function(_, bufnr)
+    -- Format on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ async = false })
+      end,
+    })
+  end,
+})
