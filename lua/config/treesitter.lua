a38bbff166a84646113f69e4130ee9164e097dca
Trevor John <trevorrjohn@gmail.com>
2025-10-29 13:47:11 -0400

    Add elixir for treesitter

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "lua", "ruby", "javascript", "tsx", "json", "go", "zig", "c", "vim", "markdown" },
+  ensure_installed = { "lua", "ruby", "javascript", "tsx", "json", "go", "zig", "c", "vim", "markdown", "elixir", "heex" },
   highlight = { enable = true, },
   indent = { enable = true, },
 }
