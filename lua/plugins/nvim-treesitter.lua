ca79b5e2247caeb1a8342c84c3152ba5cabc6194
Trevor John <trevor@trevorjohn.com>
2026-05-12 16:07:54 -0400

    Fix nvim-treesitter setup and remove broken manual config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -4,5 +4,25 @@ return {
     branch = "main",
     lazy = false,
     build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").setup({
+        ensure_installed = {
+          "lua",
+          "ruby",
+          "javascript",
+          "tsx",
+          "json",
+          "go",
+          "zig",
+          "c",
+          "vim",
+          "markdown",
+          "elixir",
+          "heex",
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
   },
 }
